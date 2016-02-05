db9f4cadcc889d9be3cd21bb8ce30b7bb5c163b1
Varun <rapidwein@gmail.com>
2016-02-04 22:23:51 -0500

    renamed file and modified readme

--- scripts/setup.py
+++ scripts/setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup
 
-setup(name='ftp_parser',
+setup(name='sec_ftp_fetch',
       version='1.0',
       description='parse SEC document from their ftp server',
       url='http://github.com/shaowns/CSC519-SEC_Footnotes',
